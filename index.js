e0e59da0e368c1364285ed4d33ab614082ef7f98
opablomonteiro <63929281+opablomonteiro@users.noreply.github.com>
2021-12-02 18:17:41 -0300

    Perfil e Home ATT

--- index.js
+++ index.js
@@ -36,50 +36,50 @@ app.get('/perfil', async (req,res) => {
   try{
     try{
       return res.render('perfil.ejs', {name: user.name, genero: user.genero, bio: user.bio,
-        bountyName: bountyP[0].name,bountyDescription: bountyP[0].description,bountyRestrictions: bountyP[0].restrictions, bounty1: '/bounty1',
-        bountyName2: bountyP[1].name,bountyDescription2: bountyP[1].description,bountyRestrictions2: bountyP[1].restrictions, bounty2: '/bounty2',
-        bountyName3: bountyP[2].name,bountyDescription3: bountyP[2].description,bountyRestrictions3: bountyP[2].restrictions, bounty3: '/bounty3',
-        bountyName4: bountyP[3].name,bountyDescription4: bountyP[3].description,bountyRestrictions4: bountyP[3].restrictions, bounty4: '/bounty4'});
+        bountyName: bountyP[0].name,bountyDescription: bountyP[0].description,bountyRestrictions: bountyP[0].restrictions, bountyPerfil1: '/bountyPerfil1',
+        bountyName2: bountyP[1].name,bountyDescription2: bountyP[1].description,bountyRestrictions2: bountyP[1].restrictions, bountyPerfil2: '/bountyPerfil2',
+        bountyName3: bountyP[2].name,bountyDescription3: bountyP[2].description,bountyRestrictions3: bountyP[2].restrictions, bountyPerfil3: '/bountyPerfil3',
+        bountyName4: bountyP[3].name,bountyDescription4: bountyP[3].description,bountyRestrictions4: bountyP[3].restrictions, bountyPerfil4: '/bountyPerfil4'});
     }catch(err){
       console.log('primeiro try');
     }
 
     try{
       return res.render('perfil.ejs', {name: user.name, genero: user.genero, bio: user.bio,
-        bountyName: bountyP[0].name,bountyDescription: bountyP[0].description,bountyRestrictions: bountyP[0].restrictions, bounty1: '/bounty1',
-        bountyName2: bountyP[1].name,bountyDescription2: bountyP[1].description,bountyRestrictions2: bountyP[1].restrictions, bounty2: '/bounty2',
-        bountyName3: bountyP[2].name,bountyDescription3: bountyP[2].description,bountyRestrictions3: bountyP[2].restrictions, bounty3: '/bounty3',
-        bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', bounty4: ''});
+        bountyName: bountyP[0].name,bountyDescription: bountyP[0].description,bountyRestrictions: bountyP[0].restrictions, bountyPerfil1: '/bountyPerfil1',
+        bountyName2: bountyP[1].name,bountyDescription2: bountyP[1].description,bountyRestrictions2: bountyP[1].restrictions, bounty2Perfil: '/bountyPerfil2',
+        bountyName3: bountyP[2].name,bountyDescription3: bountyP[2].description,bountyRestrictions3: bountyP[2].restrictions, bountyPerfil3: '/bountyPerfil3',
+        bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', bountyPerfil4: ''});
     }catch(err){
       console.log('segundo try');
     }
 
     try{
       return res.render('perfil.ejs', {name: user.name, genero: user.genero, bio: user.bio,
-        bountyName: bountyP[0].name,bountyDescription: bountyP[0].description,bountyRestrictions: bountyP[0].restrictions, bounty1: '/bounty1',
-        bountyName2: bountyP[1].name,bountyDescription2: bountyP[1].description,bountyRestrictions2: bountyP[1].restrictions, bounty2: '/bounty2',
-        bountyName3: '',bountyDescription3: '',bountyRestrictions3: '', bounty3: '',
-        bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', bounty4: ''});
+        bountyName: bountyP[0].name,bountyDescription: bountyP[0].description,bountyRestrictions: bountyP[0].restrictions, bountyPerfil1: '/bountyPerfil1',
+        bountyName2: bountyP[1].name,bountyDescription2: bountyP[1].description,bountyRestrictions2: bountyP[1].restrictions, bountyPerfil2: '/bountyPerfil2',
+        bountyName3: '',bountyDescription3: '',bountyRestrictions3: '', bountyPerfil3: '',
+        bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', bountyPerfil4: ''});
     }catch(err){
       console.log('terceiro try');
     }
 
     try{
       return res.render('perfil.ejs', {name: user.name, genero: user.genero, bio: user.bio,
-        bountyName: bountyP[0].name,bountyDescription: bountyP[0].description,bountyRestrictions: bountyP[0].restrictions, bounty1: '/bounty1',
-        bountyName2: '',bountyDescription2: '',bountyRestrictions2: '', bounty2: '',
-        bountyName3: '',bountyDescription3: '',bountyRestrictions3: '', bounty3: '',
-        bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', bounty4: ''});
+        bountyName: bountyP[0].name,bountyDescription: bountyP[0].description,bountyRestrictions: bountyP[0].restrictions, bountyPerfil1: '/bounty1',
+        bountyName2: '',bountyDescription2: '',bountyRestrictions2: '', bountyPerfil2: '',
+        bountyName3: '',bountyDescription3: '',bountyRestrictions3: '', bountyPerfil3: '',
+        bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', bountyPerfil4: ''});
     }catch(err){
       console.log('quarto try');
     }
 
     try{
       return res.render('perfil.ejs', {name: user.name, genero: user.genero, bio: user.bio,
-        bountyName: '',bountyDescription: '',bountyRestrictions: '', bounty1: '',
-        bountyName2: '',bountyDescription2: '',bountyRestrictions2: '', bounty2: '',
-        bountyName3: '',bountyDescription3: '',bountyRestrictions3: '', bounty3: '',
-        bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', bounty4: ''});
+        bountyName: '',bountyDescription: '',bountyRestrictions: '', bountyPerfil1: '',
+        bountyName2: '',bountyDescription2: '',bountyRestrictions2: '', bountyPerfil2: '',
+        bountyName3: '',bountyDescription3: '',bountyRestrictions3: '', bountyPerfil3: '',
+        bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', bountyPerfil4: ''});
     }catch(err){
       console.log('quinto try');
     }
@@ -116,16 +116,16 @@ app.get('/historico', async (req,res) => {
   res.render('historico.ejs', {name: user.name, genero: user.genero, bio: user.bio});
 });
 
-app.get('/bounty1', async (req,res) => {
+app.get('/bountyPerfil1', async (req,res) => {
   const  email  = usermail;
   const user = await User.findOne({ email: email });
   
   const bountyP = await Bounty.findOne({ _id: bounty1ID });
-  console.log(bountyP)
+  
   try{
 
     try{
-      return res.render('bounty.ejs', {name: user.name, bountyName: bountyP.name, bountyPrazoFinal: bountyP.dataFinal, bountyRestricoes: bountyP.restrictions, bountyDescricao: bountyP.description});
+      return res.render('bountyPerfil.ejs', {name: user.name, bountyName: bountyP.name, bountyPrazoFinal: bountyP.dataFinal, bountyRestricoes: bountyP.restrictions, bountyDescricao: bountyP.description});
     }catch(err){
       console.log(err);
     }
@@ -134,17 +134,55 @@ app.get('/bounty1', async (req,res) => {
   
 });
 
-app.get('/bounty2', async (req,res) => {
+app.get('/bountyPerfil2', async (req,res) => {
   const  email  = usermail;
   const user = await User.findOne({ email: email });
   
   const bountyP = await Bounty.findOne({ _id: bounty2ID });
-  console.log(bountyP)
+  
   try{
 
     try{
       
-      return res.render('bounty.ejs', {name: user.name, bountyName: bountyP.name, bountyPrazoFinal: bountyP.dataFinal, bountyRestricoes: bountyP.restrictions, bountyDescricao: bountyP.description});
+      return res.render('bountyPerfil.ejs', {name: user.name, bountyName: bountyP.name, bountyPrazoFinal: bountyP.dataFinal, bountyRestricoes: bountyP.restrictions, bountyDescricao: bountyP.description});
+    }catch(err){
+      console.log(err);
+    }
+  }catch(err){console.log('finally')}
+
+  
+});
+
+app.get('/bountyPerfil3', async (req,res) => {
+  const  email  = usermail;
+  const user = await User.findOne({ email: email });
+  
+  const bountyP = await Bounty.findOne({ _id: bounty3ID });
+  
+  try{
+
+    try{
+      
+      return res.render('bountyPerfil.ejs', {name: user.name, bountyName: bountyP.name, bountyPrazoFinal: bountyP.dataFinal, bountyRestricoes: bountyP.restrictions, bountyDescricao: bountyP.description});
+    }catch(err){
+      console.log(err);
+    }
+  }catch(err){console.log('finally')}
+
+  
+});
+
+app.get('/bountyPerfil4', async (req,res) => {
+  const  email  = usermail;
+  const user = await User.findOne({ email: email });
+  
+  const bountyP = await Bounty.findOne({ _id: bounty4ID });
+  
+  try{
+
+    try{
+      
+      return res.render('bountyPerfil.ejs', {name: user.name, bountyName: bountyP.name, bountyPrazoFinal: bountyP.dataFinal, bountyRestricoes: bountyP.restrictions, bountyDescricao: bountyP.description});
     }catch(err){
       console.log(err);
     }
@@ -198,40 +236,40 @@ app.get('/home', async (req,res) => {
 
       try{
         return res.render('home.ejs', {name: user.name, genero: user.genero, bio: user.bio,
-          bountyName: bounty[0].name,bountyDescription: bounty[0].description,bountyRestrictions: bounty[0].restrictions, home1: '/home1',
-          bountyName2: bounty[1].name,bountyDescription2: bounty[1].description,bountyRestrictions2: bounty[1].restrictions, home2: '/home2',
-          bountyName3: bounty[2].name,bountyDescription3: bounty[2].description,bountyRestrictions3: bounty[2].restrictions, home3: '/home3',
-          bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', home4: ''});
+          bountyName: bounty[0].name,bountyDescription: bounty[0].description,bountyRestrictions: bounty[0].restrictions, bounty1: '/bounty1',
+          bountyName2: bounty[1].name,bountyDescription2: bounty[1].description,bountyRestrictions2: bounty[1].restrictions, bounty2: '/bounty2',
+          bountyName3: bounty[2].name,bountyDescription3: bounty[2].description,bountyRestrictions3: bounty[2].restrictions, bounty3: '/bounty3',
+          bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', bounty4: ''});
       }catch(err){
         console.log('segundo try');
       }
 
       try{
         return res.render('home.ejs', {name: user.name, genero: user.genero, bio: user.bio,
-          bountyName: bounty[0].name,bountyDescription: bounty[0].description,bountyRestrictions: bounty[0].restrictions, home1: '/home1',
-          bountyName2: bounty[1].name,bountyDescription2: bounty[1].description,bountyRestrictions2: bounty[1].restrictions, home2: '/home2',
-          bountyName3: '',bountyDescription3: '',bountyRestrictions3: '', home3: '',
-          bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', home4: ''});
+          bountyName: bounty[0].name,bountyDescription: bounty[0].description,bountyRestrictions: bounty[0].restrictions, bounty1: '/bounty1',
+          bountyName2: bounty[1].name,bountyDescription2: bounty[1].description,bountyRestrictions2: bounty[1].restrictions, bounty2: '/bounty2',
+          bountyName3: '',bountyDescription3: '',bountyRestrictions3: '', bounty3: '',
+          bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', bounty4: ''});
       }catch(err){
         console.log('terceiro try');
       } 
 
       try{
         return res.render('home.ejs', {name: user.name, genero: user.genero, bio: user.bio,
-          bountyName: bounty[0].name,bountyDescription: bounty[0].description,bountyRestrictions: bounty[0].restrictions, home1: '/home1',
-          bountyName2: '',bountyDescription2: '',bountyRestrictions2: '', home2: '',
-          bountyName3: '',bountyDescription3: '',bountyRestrictions3: '', home3: '',
-          bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', home4: ''});
+          bountyName: bounty[0].name,bountyDescription: bounty[0].description,bountyRestrictions: bounty[0].restrictions, bounty1: '/bounty1',
+          bountyName2: '',bountyDescription2: '',bountyRestrictions2: '', bounty2: '',
+          bountyName3: '',bountyDescription3: '',bountyRestrictions3: '', bounty3: '',
+          bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', bounty4: ''});
       }catch(err){
         console.log('quarto try');
       }
       
       try{
         return res.render('home.ejs', {name: user.name, genero: user.genero, bio: user.bio,
-          bountyName: '',bountyDescription: '',bountyRestrictions: '', home1: '',
-          bountyName2: '',bountyDescription2: '',bountyRestrictions2: '', home2: '',
-          bountyName3: '',bountyDescription3: '',bountyRestrictions3: '', home3: '',
-          bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', home4: ''});
+          bountyName: '',bountyDescription: '',bountyRestrictions: '', bounty1: '',
+          bountyName2: '',bountyDescription2: '',bountyRestrictions2: '', bounty2: '',
+          bountyName3: '',bountyDescription3: '',bountyRestrictions3: '', bounty3: '',
+          bountyName4: '',bountyDescription4: '',bountyRestrictions4: '', bounty4: ''});
       }catch(err){
         console.log('quinto try');
       }
@@ -242,7 +280,7 @@ app.get('/home', async (req,res) => {
      
 });
 
-app.get('/home1', async (req,res) => {
+app.get('/bounty1', async (req,res) => {
   const  email  = usermail;
   const user = await User.findOne({ email: email });
   
@@ -258,7 +296,7 @@ app.get('/home1', async (req,res) => {
   
 });
 
-app.get('/home2', async (req,res) => {
+app.get('/bounty2', async (req,res) => {
   const  email  = usermail;
   const user = await User.findOne({ email: email });
   
@@ -275,7 +313,7 @@ app.get('/home2', async (req,res) => {
   
 });
 
-app.get('/home3', async (req,res) => {
+app.get('/bounty3', async (req,res) => {
   const  email  = usermail;
   const user = await User.findOne({ email: email });
   
@@ -292,7 +330,7 @@ app.get('/home3', async (req,res) => {
   
 });
 
-app.get('/home4', async (req,res) => {
+app.get('/bounty4', async (req,res) => {
   const  email  = usermail;
   const user = await User.findOne({ email: email });
   
